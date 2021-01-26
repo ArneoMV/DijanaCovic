8221de945e6b7b16722e138bd63ec5c84e375cc3
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2021-01-26 15:23:19 +0100

    TODO
    Navbar, exhibitions, products, about

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -4,23 +4,12 @@ $(document).ready(function() {
   const menuSlide = () => {
     const menuIcon = document.querySelector(".menu-icon");
     const navLinks = document.querySelector(".nav-links");
-    const navLinksInner = document.querySelectorAll(".nav-links a");
+    //const navLinksInner = document.querySelectorAll(".nav-links a");
   
     //menu-icon click event
     menuIcon.addEventListener("click", () => {
       //toggle active class
       navLinks.classList.toggle("menu-active");
-  
-      //animate navLinks
-      // navLinksInner.forEach((a, index) => {
-      //   if (a.style.animation) {
-      //     a.style.animation = "";
-      //   } else {
-      //     a.style.animation = `navLinkAnime 0.5s ease forwards ${
-      //       index / 7 + 0.3
-      //     }s`;
-      //   }
-      // });
       //toggle for menu-icon animation
       menuIcon.classList.toggle("span-anime");
     });
@@ -29,17 +18,6 @@ $(document).ready(function() {
     navLinks.addEventListener("click", () => {
       //toggle active class
       navLinks.classList.toggle("menu-active");
-  
-      //animate navLinks
-      // navLinksInner.forEach((a, index) => {
-      //   if (a.style.animation) {
-      //     a.style.animation = "";
-      //   } else {
-      //     a.style.animation = `navLinkAnime 0.5s ease forwards ${
-      //       index / 7 + 0.3
-      //     }s`;
-      //   }
-      // });
       //toggle for menu-icon animation
       menuIcon.classList.toggle("span-anime");
     });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,44 +1,89 @@
+// NAVIGATION
 $(document).ready(function() {
- // NAVIGATION
- $("#main").load("html/Homepage.html");
- $('.nav-home').click(function() { 
-  $("#main").load("html/Homepage.html");
-  $(".nav-home").addClass("link_active");
-  $(".nav-gallery").removeClass("link_active");
-  $(".nav-exhibitions").removeClass("link_active");
-  $(".nav-products").removeClass("link_active");
-  $(".nav-about").removeClass("link_active");
- });
- $('.nav-gallery').click(function() {
-  $('#main').load("html/Gallery.html");
-  $(".nav-gallery").addClass("link_active");
-  $(".nav-home").removeClass("link_active");
-  $(".nav-exhibitions").removeClass("link_active");
-  $(".nav-products").removeClass("link_active");
-  $(".nav-about").removeClass("link_active");
- });
- $('.nav-exhibitions').click(function() {
-  $('#main').load("html/Exhibitions.html");
-  $(".nav-exhibitions").addClass("link_active");
-  $(".nav-home").removeClass("link_active");
-  $(".nav-gallery").removeClass("link_active");
-  $(".nav-products").removeClass("link_active");
-  $(".nav-about").removeClass("link_active");
+
+$("#main").load("html/Homepage.html");
+// ActivitiesContent Navigacija
+function navbarLinks(ob_navigateTo) {
+  let navbarLinksID = [".nav-home",".nav-gallery", ".nav-exhibitions", ".nav-products", ".nav-about"];
+
+  for (let index = 0; index < navbarLinksID.length; index++) {
+    if (ob_navigateTo == navbarLinksID[index]) {
+      // $(ob_navigateTo).addClass("link_active");
+      $(ob_navigateTo).addClass("link_active");
+    }
+    else{
+      // $(navbarLinksID[index]).removeClass("link_active");
+      $(navbarLinksID[index]).removeClass("link_active");
+    }
+  }
+}
+
+  $('.nav-home').click(function() { 
+    $("#main").load("html/Homepage.html");
+    navbarLinks(".nav-home");
+  });
+
+  $('.nav-gallery').click(function() {  
+    $('#main').load("html/Gallery.html");
+    navbarLinks(".nav-gallery");
+  });
+
+  $('.nav-exhibitions').click(function() {  
+    $('#main').load("html/Exhibitions.html");  
+    navbarLinks(".nav-exhibitions");
+  });
+
+  $('.nav-products').click(function(){  
+    $('#main').load("html/Products.html");  
+    navbarLinks(".nav-products");
+  });
+
+  $('.nav-about').click(function(){  
+    $('#main').load("html/About.html");  
+    navbarLinks(".nav-about");
+  });
 });
- $('.nav-products').click(function(){
-  $('#main').load("html/Products.html");
-  $(".nav-products").addClass("link_active");
-  $(".nav-home").removeClass("link_active");
-  $(".nav-gallery").removeClass("link_active");
-  $(".nav-exhibitions").removeClass("link_active");
-  $(".nav-about").removeClass("link_active");
- });
- $('.nav-about').click(function(){
-  $('#main').load("html/About.html");
-  $(".nav-about").addClass("link_active");
-  $(".nav-home").removeClass("link_active");
-  $(".nav-gallery").removeClass("link_active");
-  $(".nav-exhibitions").removeClass("link_active");
-  $(".nav-products").removeClass("link_active");
- });
-});
+
+
+//  $('.nav-home').click(function() { 
+//   $("#main").load("html/Homepage.html");
+//   $(".nav-home").addClass("link_active");
+//   $(".nav-gallery").removeClass("link_active");
+//   $(".nav-exhibitions").removeClass("link_active");
+//   $(".nav-products").removeClass("link_active");
+//   $(".nav-about").removeClass("link_active");
+//  });
+
+//  $('.nav-gallery').click(function() {
+//   $('#main').load("html/Gallery.html");
+//   $(".nav-gallery").addClass("link_active");
+//   $(".nav-home").removeClass("link_active");
+//   $(".nav-exhibitions").removeClass("link_active");
+//   $(".nav-products").removeClass("link_active");
+//   $(".nav-about").removeClass("link_active");
+//  });
+
+//  $('.nav-exhibitions').click(function() {
+//   $('#main').load("html/Exhibitions.html");
+//   $(".nav-exhibitions").addClass("link_active");
+//   $(".nav-home").removeClass("link_active");
+//   $(".nav-gallery").removeClass("link_active");
+//   $(".nav-products").removeClass("link_active");
+//   $(".nav-about").removeClass("link_active");
+// });
+//  $('.nav-products').click(function(){
+//   $('#main').load("html/Products.html");
+//   $(".nav-products").addClass("link_active");
+//   $(".nav-home").removeClass("link_active");
+//   $(".nav-gallery").removeClass("link_active");
+//   $(".nav-exhibitions").removeClass("link_active");
+//   $(".nav-about").removeClass("link_active");
+//  });
+//  $('.nav-about').click(function(){
+//   $('#main').load("html/About.html");
+//   $(".nav-about").addClass("link_active");
+//   $(".nav-home").removeClass("link_active");
+//   $(".nav-gallery").removeClass("link_active");
+//   $(".nav-exhibitions").removeClass("link_active");
+//   $(".nav-products").removeClass("link_active");
+//  });
