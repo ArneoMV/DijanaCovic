a7378a62b0768834cce0d53c240bfdc1942f47ba
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2021-02-13 12:08:40 +0100

    navigacija

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,41 +5,41 @@ $("#header").load("html/Header.html");
 $("#main").load("html/Homepage.html");
 
 // ActivitiesContent Navigacija
-function navbarLinks(navigateTo) {
-  let navbarLinksID = [".nav-home",".nav-gallery", ".nav-exhibitions", ".nav-products", ".nav-about"];
+// function navbarLinks(navigateTo) {
+//   let navbarLinksID = [".nav-home",".nav-gallery", ".nav-exhibitions", ".nav-products", ".nav-about"];
 
-  for (let index = 0; index < navbarLinksID.length; index++) {
-    if (navigateTo == navbarLinksID[index]) {
-      $(navigateTo).addClass("link_active");
-    }
-    else{
-      $(navbarLinksID[index]).removeClass("link_active");
-    }
-  }
-}
+//   for (let index = 0; index < navbarLinksID.length; index++) {
+//     if (navigateTo == navbarLinksID[index]) {
+//       $(navigateTo).addClass("link_active");
+//     }
+//     else{
+//       $(navbarLinksID[index]).removeClass("link_active");
+//     }
+//   }
+// }
 
-  $('.nav-home').click(function() { 
-    $("#main").load("html/Homepage.html");
-    navbarLinks(".nav-home");
-  });
+//   $('.nav-home').click(function() { 
+//     $("#main").load("html/Homepage.html");
+//     navbarLinks(".nav-home");
+//   });
 
-  $('.nav-gallery').click(function() {  
-    $('#main').load("html/Gallery.html");
-    navbarLinks(".nav-gallery");
-  });
+//   $('.nav-gallery').click(function() {  
+//     $('#main').load("html/Gallery.html");
+//     navbarLinks(".nav-gallery");
+//   });
 
-  $('.nav-exhibitions').click(function() {  
-    $('#main').load("html/Exhibitions.html");  
-    navbarLinks(".nav-exhibitions");
-  });
+//   $('.nav-exhibitions').click(function() {  
+//     $('#main').load("html/Exhibitions.html");  
+//     navbarLinks(".nav-exhibitions");
+//   });
 
-  $('.nav-products').click(function(){  
-    $('#main').load("html/Products.html");  
-    navbarLinks(".nav-products");
-  });
+//   $('.nav-products').click(function(){  
+//     $('#main').load("html/Products.html");  
+//     navbarLinks(".nav-products");
+//   });
 
-  $('.nav-about').click(function(){  
-    $('#main').load("html/About.html");  
-    navbarLinks(".nav-about");
-  });
+//   $('.nav-about').click(function(){  
+//     $('#main').load("html/About.html");  
+//     navbarLinks(".nav-about");
+//   });
 });
