b1457f260f31ee7e3bce0df22652ec7f46313c67
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2021-03-22 19:30:54 +0100

    gallery / painting

--- public/scripts/Gallery_Page.js
+++ public/scripts/Gallery_Page.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 
- $( "#img-1" ).click(function() {
-  $("#main").load("html/Painting.html");
- });
+    // SHOW/HIDE FILTERS 
+    $(".filter_icon").click(function() {
+        $("form").toggleClass("filter_active");
+    });
 
- // SHOW/HIDE FILTERS 
- $(".filter_icon").click(function() {
-  $("form").toggleClass("filter_active");
- });
 });
