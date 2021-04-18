e9b550e06cf7f1b3a81b4149a49cdf15779f73d2
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2021-04-18 15:23:18 +0200

    navbar

--- public/scripts/NavBar.js
+++ public/scripts/NavBar.js
@@ -1,19 +1,74 @@
 
+
 $(document).ready(function() {
 
-    let burger = document.querySelector('.burger');
-    let navMobile = document.querySelector('.nav-mobile');
-    let navBar = document.getElementById("navBar");
-    
-    burger.addEventListener('click', () => {
+  const body = document.querySelector("body");
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-      //alert("klik");
-      
-      // Burger Klick
-      burger.classList.toggle('toggle');
-      navMobile.classList.toggle('is-active');
-      navBar.style.top = "0";
-      document.body.classList.toggle('overflowHidden');
-    });  
 
+  // Navigation
+  const navSlide = () => {
+    burger.addEventListener("click", () => {
+      //Toggle Nav
+      nav.classList.toggle("nav-active");
+      //Body overflow fixed
+      // body.classList.toggle("fixed");
+
+     //Animate Links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+      });
+      //Burger Animation
+      burger.classList.toggle("toggle");
+    });
+  }
+  navSlide();
+
+    // Switch pages
+    // $('#Home').click(function() {
+    //   $('#page').load('html/Homepage.html');
+    //   nav.classList.toggle("nav-active");
+    //   burger.classList.toggle("toggle");
+    // });
+    // $('#Solutions').click(function() {
+    //   $('#page').load('html/Solutions.html');
+    //   nav.classList.toggle("nav-active");
+    //   burger.classList.toggle("toggle");
+    // });
+    // $('#IoT').click(function() {
+    //   $('#page').load('html/IoT.html');
+    //   nav.classList.toggle("nav-active");
+    //   burger.classList.toggle("toggle");
+    // });
+    // $('#Project').click(function() {
+    //   $('#page').load('html/Project.html');
+    //   nav.classList.toggle("nav-active");
+    //   burger.classList.toggle("toggle");
+    // });
 });
+
+
+// $(document).ready(function() {
+
+//     let burger = document.querySelector('.burger');
+//     let navMobile = document.querySelector('.nav-mobile');
+//     let navBar = document.getElementById("navBar");
+    
+//     burger.addEventListener('click', () => {
+
+//       //alert("klik");
+      
+//       // Burger Klick
+//       burger.classList.toggle('toggle');
+//       navMobile.classList.toggle('is-active');
+//       navBar.style.top = "0";
+//       document.body.classList.toggle('overflowHidden');
+//     });  
+
+// });
